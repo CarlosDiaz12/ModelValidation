39ea87498a4f3343492b617072e817eefddf09c4
Carlos Diaz <carlos.cedp@gmail.com>
2022-02-08 15:50:31 -0400

    page 678

--- Models/Appointment.cs
+++ Models/Appointment.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ModelValidation.Infrastructure;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -8,9 +9,15 @@ namespace ModelValidation.Models
 {
     public class Appointment
     {
+        [Required]
         public string ClientName { get; set; }
+        [Required(ErrorMessage = "Please enter a date")]
         [DataType(DataType.Date)]
+        [FutureDate(ErrorMessage = "Please enter a date in the future")]
         public DateTime Date { get; set; }
+        // Akward
+        // [Range(typeof(bool), "true", "true", ErrorMessage = "You must aaccept the terms")]
+        [MustBeTrue(ErrorMessage = "You must acept the terms")]
         public bool TermsAccepted { get; set; }
     }
 }
