6d5b0148d44cadfa433a45a3be77472ee0e7b0c7
Carlos Diaz <carlos.cedp@gmail.com>
2022-02-08 16:11:00 -0400

    page 682

--- Models/Appointment.cs
+++ Models/Appointment.cs
@@ -9,17 +9,33 @@ namespace ModelValidation.Models
 {
     // Applying a Model-Level Custom Validation Attribute
     [NoJoeOnMondays]
-    public class Appointment
+    public class Appointment: IValidatableObject
     {
-        [Required]
         public string ClientName { get; set; }
-        [Required(ErrorMessage = "Please enter a date")]
         [DataType(DataType.Date)]
-        [FutureDate(ErrorMessage = "Please enter a date in the future")]
         public DateTime Date { get; set; }
-        // Akward
-        // [Range(typeof(bool), "true", "true", ErrorMessage = "You must aaccept the terms")]
-        [MustBeTrue(ErrorMessage = "You must acept the terms")]
         public bool TermsAccepted { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            List<ValidationResult> errors = new List<ValidationResult>();
+            if (string.IsNullOrEmpty(ClientName))
+            {
+                errors.Add(new ValidationResult("Please enter your name"));
+            }
+            if (DateTime.Now > Date)
+            {
+                errors.Add(new ValidationResult("Please enter a date in the future"));
+            }
+            if (errors.Count == 0 && ClientName == "Joe" && Date.DayOfWeek == DayOfWeek.Monday)
+            {
+                errors.Add( new ValidationResult("Joe cannot book appointments on Mondays"));
+            }
+            if (!TermsAccepted)
+            {
+                errors.Add(new ValidationResult("You must accept the terms"));
+            }
+            return errors;
+        }
     }
 }
