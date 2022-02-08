68670ccae2a4a7f9db4dfd3f2ef71d088d6bc3de
Carlos Diaz <carlos.cedp@gmail.com>
2022-02-08 16:57:00 -0400

    client-side validation, page 686

--- Models/Appointment.cs
+++ Models/Appointment.cs
@@ -7,35 +7,13 @@ using System.Web;
 
 namespace ModelValidation.Models
 {
-    // Applying a Model-Level Custom Validation Attribute
-    [NoJoeOnMondays]
-    public class Appointment: IValidatableObject
+    public class Appointment
     {
+        [Required]
+        [StringLength(10, MinimumLength = 3)]
         public string ClientName { get; set; }
         [DataType(DataType.Date)]
         public DateTime Date { get; set; }
         public bool TermsAccepted { get; set; }
-
-        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
-        {
-            List<ValidationResult> errors = new List<ValidationResult>();
-            if (string.IsNullOrEmpty(ClientName))
-            {
-                errors.Add(new ValidationResult("Please enter your name"));
-            }
-            if (DateTime.Now > Date)
-            {
-                errors.Add(new ValidationResult("Please enter a date in the future"));
-            }
-            if (errors.Count == 0 && ClientName == "Joe" && Date.DayOfWeek == DayOfWeek.Monday)
-            {
-                errors.Add( new ValidationResult("Joe cannot book appointments on Mondays"));
-            }
-            if (!TermsAccepted)
-            {
-                errors.Add(new ValidationResult("You must accept the terms"));
-            }
-            return errors;
-        }
     }
 }
